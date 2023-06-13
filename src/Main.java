fa5751efe6446babc1351d021bb7d6b1df738ecc
Davide Francesco Riccio <ledrazors@gmail.com>
2023-06-13 15:13:54 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String[] carBrands = {"Ferrari","Porsche","Fiat","Ford","Peugeot"};
+
+
+
+        System.out.println("L'array carBrands contiene " + carBrands.length + " elementi.");
+        System.out.println(carBrands[2]);
+
+        int[] primeNumbers = {2,3,5,7,11,13};
+
+        System.out.println(Arrays.toString(primeNumbers));
+
+
+    }
+}
